feafc5231aa90d1ee137cd5c583f3c9fabb18922
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-07-02 08:44:36 +0300

    Add files via upload

---
+++ Pipes/src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.Pipe;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try{
+			Pipe pipe = Pipe.open();
+			Runnable writer = new Runnable() {
+				@Override
+				public void run() {
+					try{
+						Pipe.SinkChannel sinkChannel = pipe.sink();
+						ByteBuffer buffer = ByteBuffer.allocate(56);
+						for(int i=0 ; i<10; i++){
+							String currentTime = "The time is: " + System.currentTimeMillis();
+							buffer.put(currentTime.getBytes(StandardCharsets.UTF_8));
+							buffer.flip();
+							while(buffer.hasRemaining()){
+								sinkChannel.write(buffer);
+							}
+							buffer.flip();
+							Thread.sleep(100);
+						}
+					}catch (Exception e){
+						e.printStackTrace();
+					}
+				}
+			};
+			Runnable reader = new Runnable() {
+				@Override
+				public void run() {
+			    try{
+			    	Pipe.SourceChannel sourceChannel = pipe.source();
+			    	ByteBuffer buffer = ByteBuffer.allocate(56);
+
+			    	for(int i=0; i<10; i++){
+			    		int bytesRead = sourceChannel.read(buffer);
+			    		byte[] timeString = new byte[bytesRead];
+			    		buffer.flip();
+			    		buffer.get(timeString);
+						System.out.println("Reader Thread: " + new String(timeString));
+						buffer.flip();
+						Thread.sleep(100);
+					}
+				}catch(Exception e){
+			    	e.printStackTrace();
+				}
+				}
+			};
+			new Thread(writer).start();
+			new Thread(reader).start();
+
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+}
+
